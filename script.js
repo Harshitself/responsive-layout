b7c7f07d3eec470142d9db89dcd9f772c43032cf
Harshit Bhardwaj <88239900+Harshitself@users.noreply.github.com>
2022-07-29 09:59:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const togbtn = document.getElementById('tgn');
+const navlist = document.getElementById('nav-list');
+
+togbtn.addEventListener('click', () => {
+   navlist.classList.toggle('open');
+
+})
+
+// const xyz =document.getElementsByClassName('toggleNav');
+// const abc =document.getElementsByClassName('flex-nav');
+
+// xyz.addEventListener('click',() =>{
+//     abc.classList.toggle('open')
+// }
+// )
